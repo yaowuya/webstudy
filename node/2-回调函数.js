fd378151b2592bc5ae0443c91f5ee4b0e48a26ba
yaowuya <zrfyaowuya@126.com>
2019-12-07 10:06:33 +0800

    删减

--- node/2-回调函数.js
+++
@@ -1,9 +0,0 @@
-function callFunction(fun,name) {
-    fun(name);
-}
-
-var sayBye=function (name) {
-    console.log(name+"bye");
-}
-
-callFunction(sayBye,"123");
--- node/write.js
+++
@@ -1,9 +0,0 @@
-const fs =require("fs");
-
-fs.writeFile("./a.txt","你好，博客",function (err) {
-    if(err){
-        console.log(err);
-        return;
-    }
-    console.log("success");
-})
